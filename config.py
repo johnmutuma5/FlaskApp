b768222e9a879b8647a221e68d3daca65cc63a90
John Mutuma <johnmutuma5@gmail.com>
2018-02-13 17:47:54 +0300

    Revert "First commit of this app"
    This reverts commit 4e24761adc4fed4f7178960e1eb9177e4fb65fc1.

--- config.py
+++
@@ -1,4 +0,0 @@
-MYSQL_DATABASE_HOST = 'localhost'
-MYSQL_DATABASE_USER = 'root'
-MYSQL_DATABASE_PASSWORD = 'M@thematic$$89'
-MYSQL_DATABASE_DB = 'venues'
--- run.py
+++
@@ -1,7 +0,0 @@
-#!flaskapp/bin/python
-
-from application import app
-
-if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 5000, app, use_debugger=True, use_reloader=True)
