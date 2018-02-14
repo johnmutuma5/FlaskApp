7edb6fa449fb4b119999cab9956162eb041d6c08
JohnM <johnmutuma5@gmail.com>
2018-02-14 12:34:51 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 from application import app
 
+#run this when this file is main
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('0.0.0.0', 5000, app, use_debugger=True, use_reloader=True)
